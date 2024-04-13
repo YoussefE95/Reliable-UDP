22ebfab1e632eb204deea2e76394f934361e4015
Youssef <youssef95elzein@gmail.com>
2024-04-13 00:22:56 -0700

    better organized client

--- client.py
+++ client.py
@@ -11,37 +11,38 @@ def send(mesg, addr, port, seqRange):
         socket.sendto(encapsulate(seq, mesg).encode(), (addr, port))
 
 def recv(batch):
-    start = time.perf_counter()
-    while(len(batch) > 0 and time.perf_counter() - start < timeout):
-        resp, respAddr = socket.recvfrom(2048)
-        ack, respMsg = decapsulate(resp.decode())
- 
-        if (ack in batch):
-            batch.remove(ack)
-            print(ack, respMsg)
-
-dest = input('Destination: ')
-mesg = input('Message: ')
-total = int(input('Number times to send: '))
-
-addr = dest.split(':')[0]
-port = int(dest.split(':')[1])
-
-print('\n-------------------------------------------')
-for i in range(0, total, window):
-    batchLength = i + window
-
-    if (batchLength > total):
-        batchLength = total
-    
-    seqRange = range(i, batchLength)
-    batch = list(seqRange)
-
-    send(mesg, addr, port, seqRange)
-    
     while(len(batch) > 0):
-        recv(batch)
+        start = time.perf_counter()
+        while(len(batch) > 0 and time.perf_counter() - start < timeout):
+            data, addr = socket.recvfrom(2048)
+            ack, mesg = decapsulate(data.decode())
+     
+            if (ack in batch):
+                batch.remove(ack)
+                print(ack, mesg)
 
-    print('-------------------------------------------')
+def main():
+    dest = input('Destination: ')
+    mesg = input('Message: ')
+    total = int(input('Number times to send: '))
 
-socket.close()
+    addr = dest.split(':')[0]
+    port = int(dest.split(':')[1])
+
+    print('\n-------------------------------------------')
+    for i in range(0, total, window):
+        batchLength = i + window
+
+        if (batchLength > total):
+            batchLength = total
+
+        seqRange = range(i, batchLength)
+
+        send(mesg, addr, port, seqRange)
+        recv(list(seqRange))
+        print('-------------------------------------------')
+
+    socket.close()
+
+if __name__ == '__main__':
+    main()
